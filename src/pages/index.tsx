7aee43a170b9f17558cb1ce2380eb9f52e6480ed
Andre Morpa <andremorpa@gmail.com>
2021-02-17 07:36:58 +0000

    Add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
-import Main from 'components/Main'
+import Main, { MainProps } from 'components/Main'
 
-export default function Home() {
-  return <Main />
+export default function Home(props: MainProps) {
+  return <Main {...props} />
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      title: 'Espaço para divulgação de vagas para front-enders.',
+      jobs: 345
+    }
+  }
 }
